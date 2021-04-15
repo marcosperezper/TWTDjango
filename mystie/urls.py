719e4034f5e02b456ab0e01d6f71d6caa330e68b
marcos <marcosperez@Marcos-Perez.local>
2021-04-15 09:07:15 +0200

    Login

--- mystie/urls.py
+++ mystie/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', v.register, name="register"),
     path('', include("main.urls")),
+    path('', include("django.contrib.auth.urls")),
 ]
+
